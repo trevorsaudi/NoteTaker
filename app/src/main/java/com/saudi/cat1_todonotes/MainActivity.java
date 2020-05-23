7d8f794e511f929d77d697f903b3afda8450de2b
Trevor Saudi <isatrevor052@gmail.com>
2020-05-23 20:33:18 +0300

    implemented the adpater and the recycler view

--- app/src/main/java/com/saudi/cat1_todonotes/MainActivity.java
+++ app/src/main/java/com/saudi/cat1_todonotes/MainActivity.java
@@ -1,9 +1,12 @@
 package com.saudi.cat1_todonotes;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -19,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this)); //adds notes below each other
+        recyclerView.setHasFixedSize(true); //the recycler view size does not change
+
+        final NoteAdapter adapter = new NoteAdapter(); //we use final since we access it in an inner class
+        recyclerView.setAdapter(adapter);
         /*we dont call noteViewModel since it will create another instance of viewmodel
         with every new activity, so instead we get it from the android system
            */
@@ -26,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
         //livedata
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
-            public void onChanged(List<Note> notes) { //triggered everytime the livedata changes
+            public void onChanged(@Nullable List<Note> notes) { //triggered everytime the livedata changes
                 //update RecyclerView
-                Toast.makeText(MainActivity.this,"onChanged",Toast.LENGTH_SHORT).show();
-
+                //the adapter is updated everytime obChanged is called, when new data comes
+             adapter.setNotes(notes);
             }
         });
     }
---
+++ app/src/main/java/com/saudi/cat1_todonotes/NoteAdapter.java
@@ -0,0 +1,61 @@
+package com.saudi.cat1_todonotes;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    private List<Note> notes = new ArrayList<>();
+
+    @NonNull
+    @Override
+    //this is the method that will create the view for the holder
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item, parent, false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        //get all the data from the single note objects of the holder
+        //reference the note object
+        Note currentNote = notes.get(position);
+        holder.textViewTitle.setText(currentNote.getTitle());
+        holder.textViewDescription.setText(currentNote.getDescription());
+        holder.textViewPriority.setText(String.valueOf(currentNote.getPriority())); //converts the item to a string
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes) {
+        //gets the list of notes
+        this.notes = notes;
+        notifyDataSetChanged(); //updating the adapter
+    }
+
+    //create a holder that holds the variables
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private TextView textViewPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.text_view_title);
+            textViewDescription = itemView.findViewById(R.id.text_view_description);
+            textViewPriority = itemView.findViewById(R.id.text_view_priority);
+
+        }
+    }
+}
